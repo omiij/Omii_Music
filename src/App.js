16807b1016b83be0fdda7a1339698f2f6aaf67a7
omii <onkarj544@gmail.com>
2022-02-11 05:09:12 +0530

    add

--- src/App.js
+++ src/App.js
@@ -40,7 +40,6 @@ function App() {
     <>
       {userData ? (
         <>
-<<<<<<< HEAD
           <Navbar handleLogout={handleLogout} />
 
           <Routes>
@@ -59,76 +58,6 @@ function App() {
             <Route path="/Kishorkumarsong" element={<KishorS />} />
             <Route path="/Puthsong" element={<CharliS />} />
           </Routes>
-
-          {/* <Navbar handleLogout={handleLogout} />
-=======
-           <Navbar handleLogout={handleLogout} />
-
->>>>>>> f830736be0f3478927c6b41d018fc5e29ca26e88
-          <Routes>
-            <Route path="/" element={<Artis />} />
-            <Route path="/Pitsong" element={<Pitbull />} />
-            <Route path="/Jenisong" element={<JeniferL />} />
-            <Route path="/Radfoosong" element={<RadfooS />} />
-            <Route path="/Maroonsong" element={<MaroonS />} />
-            <Route path="/Alexasong" element={<AlexandraS />} />
-            <Route path="/Akcentsong" element={<AkcentS />} />
-            <Route path="/Dadyyankeesong" element={<DaddyYS />} />
-            <Route path="/about" element={<About />} />
-<<<<<<< HEAD
-          </Routes> */}
-=======
-            <Route path="/Jubinsong" element={<JubinS />} />
-            <Route path="/Taylorsong" element={<TaylorS />} />
-            <Route path="/Brunosong" element={<BrunoS />} />
-            <Route path="/Kishorkumarsong" element={<KishorS />} />
-            <Route path="/Puthsong" element={<CharliS />} />
-          </Routes
-       
-//           <Navbar handleLogout={handleLogout} />
-//           <Routes>
-//             <Route path="/" element={<Artis />} />
-//           </Routes>
-//           <Routes>
-//             <Route path="/Pitsong" element={<Pitbull />} />
-//           </Routes>
-//           <Routes>
-//             <Route path="/Jenisong" element={<JeniferL />} />
-//           </Routes>
-//           <Routes>
-//             <Route path="/Radfoosong" element={<RadfooS />} />
-//           </Routes>
-//           <Routes>
-//             <Route path="/Maroonsong" element={<MaroonS />} />
-//           </Routes>
-//           <Routes>
-//             <Route path="/Alexasong" element={<AlexandraS />} />
-//           </Routes>
-//           <Routes>
-//             <Route path="/Akcentsong" element={<AkcentS />} />
-//           </Routes>
-//           <Routes>
-//             <Route path="/Dadyyankeesong" element={<DaddyYS />} />
-//           </Routes>
-//           <Routes>
-//             <Route path="/Kishorkumarsong" element={<KishorS />} />
-//           </Routes>
-//           <Routes>
-//             <Route path="/Puthsong" element={<CharliS />} />
-//           </Routes>
-//           <Routes>
-//             <Route path="/Brunosong" element={<BrunoS />} />
-//           </Routes>
-//           <Routes>
-//             <Route path="/Taylorsong" element={<TaylorS />} />
-//           </Routes>
-//           <Routes>
-//             <Route path="/Jubinsong" element={<JubinS />} />
-//           </Routes>
-//           <Routes>
-//             <Route path="/about" element={<About />} />
-//           </Routes>
->>>>>>> f830736be0f3478927c6b41d018fc5e29ca26e88
         </>
       ) : (
         <Login changeUserData={changeUserData} />
--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <BrowserRouter basename="/Omii_Music">
-=======
     <BrowserRouter>
->>>>>>> f830736be0f3478927c6b41d018fc5e29ca26e88
       <App />
     </BrowserRouter>
   </React.StrictMode>,
